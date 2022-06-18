dca487543baf9dbabe5a972d16c3a0410fc9fca1
Fhillip Bagsic <fhillipbagsic@Fhillips-MacBook-Pro.local>
2022-06-18 13:15:36 +0800

    Setup imports

--- server.js
+++ server.js
@@ -1,7 +1,36 @@
-import express from 'express'
+import 'express-async-errors'
+import env from 'dotenv'
+env.config()
 
+// packages
+import morgan from 'morgan'
+import cors from 'cors'
+import helmet from 'helmet'
+import { rateLimit } from 'express-rate-limit'
+
+// express
+import express from 'express'
 const app = express()
 
-const PORT = 3001
+// database
+
+
+// routers
+
+// middlewares
+
+app.use(express.json())
+
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+app.use(cors())
+app.use(helmet())
+app.use(rateLimit({windowMs: 60 * 1000, max: 150}))
+
+app.get('/', (req, res) => res.send('Depression App API'))
+
+const PORT = process.env.PORT || 5001
 
 app.listen(PORT, () => console.log(`Running on port ${PORT}`))
