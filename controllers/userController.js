263245b083bb428087f82a55e654813187f11f94
Fhillip Bagsic <fhillipbagsic@Fhillips-MacBook-Pro.local>
2022-06-24 16:36:27 +0800

    Add patient functions

---
+++ controllers/userController.js
@@ -0,0 +1,91 @@
+import { StatusCodes } from 'http-status-codes'
+import Patient from '../models/Patient.js'
+import { BadRequestError } from '../errors/index.js'
+import Clinician from '../models/Clinician.js'
+import { hashPassword } from '../utils/validatePassword.js'
+
+const createPatient = async (req, res) => {
+    const email = req.body.email
+    req.body.role = 'Patient'
+    req.body.dateAdded = new Date(Date.now())
+    req.body.assignedClinician = req.user.email
+
+    const patient = await Patient.findOne({ email })
+
+    if (patient) {
+        throw new BadRequestError('Patient is already registered')
+    }
+
+    const response = await Patient.create(req.body)
+
+    res.status(StatusCodes.CREATED).json({
+        message: `Patient ${response.firstName} ${response.lastName} has been added`,
+    })
+}
+
+const updatePatient = async (req, res) => {
+    const currentEmail = req.body.currentEmail
+    const hashedPassword = await hashPassword(req.body.password)
+
+    const response = await Patient.updateOne(
+        { email: currentEmail },
+        { ...req.body, password: hashedPassword }
+    )
+
+    res.status(StatusCodes.OK).json({ message: `Patient has been updated` })
+}
+
+const deletePatient = async (req, res) => {
+    const email = req.body.email
+
+    if (!email) {
+        throw new BadRequestError("Please provide patient's email")
+    }
+    const response = await Patient.deleteOne({ email })
+
+    res.status(StatusCodes.OK).json({
+        message: `Deleted ${response.deletedCount} patient`,
+    })
+}
+
+const getPatient = async (req, res) => {
+    const email = req.body.email
+
+    const patient = await Patient.findOne({ email })
+
+    res.status(StatusCodes.OK).json({ patient: patient })
+}
+
+const getPatients = async (req, res) => {
+    const patients = await Patient.find()
+    res.status(StatusCodes.OK).json({ patients: patients })
+}
+
+const createClinician = async (req, res) => {
+    const email = req.body.email
+
+    const clinician = await Clinician.findOne({ email })
+
+    if (clinician) {
+        throw new BadRequestError('Clinician is already registered')
+    }
+
+    Clinician.create(req.body)
+
+    res.status(StatusCodes.OK).json({ message: 'Clinician registered' })
+}
+
+const updateClinician = async (req, res) => {}
+
+const getClinician = async (req, res) => {}
+
+const getClinicians = async (req, res) => {}
+
+export {
+    createPatient,
+    updatePatient,
+    deletePatient,
+    getPatient,
+    getPatients,
+    createClinician,
+}
