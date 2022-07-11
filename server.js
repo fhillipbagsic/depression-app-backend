dd6790d396b098252dcd0a7024679e81a5f8d80d
Fhillip Bagsic <fhillipbagsic@Fhillips-MacBook-Pro.local>
2022-07-11 17:19:27 +0800

    Edit scheduler

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.use('/api/tracker', trackerRouter)
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
-const job = schedule.scheduleJob('* * 35 * *', async () => {
+const job = schedule.scheduleJob('22 17 * * *', async () => {
     const questionOfTheDay = questions[await getDay()]
 
     // const patients = await getPatientsEmails()
